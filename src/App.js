853e1560d3a6e1ea3c3af7d39b15b24a3b2a6742
mmulahvictor <mmulahvictor@gmail.com>
2023-02-18 13:25:47 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div>
-      
+    <div className='app'>
+      <Navbar />
     </div>
   );
 }
