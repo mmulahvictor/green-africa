3bfa21254e8cd8c32ee944a24811494a3cb60163
mmulahvictor <mmulahvictor@gmail.com>
2023-04-03 21:43:51 +0300

    error handling, add links to navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
@@ -5,10 +6,38 @@ import Home from './components/home/Home';
 function App() {
   return (
     <div className='app'>
-      <Navbar />
-      <Home />
+      <ErrorBoundary>
+        <Navbar />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Home />
+      </ErrorBoundary>
     </div>
   );
 }
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong.</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
 export default App;
+
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-      <div className='navbar'>
-          
+    <div className='navbar'>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/sustainable-living'>Sustainable Living</Link></li>
+        <li><Link to='/environment'>Environment</Link></li>
+        <li><Link to='/green-energy'>Green Energy</Link></li>
+        <li><Link to='/blog'>Blog</Link></li>
+        <li><Link to='/resources'>Resources</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
